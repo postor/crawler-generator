ecb7056ba50c4f5c415dd80cef3957fbe9118eb7
josh <postor@gmail.com>
2019-01-15 14:46:16 +0800

    basic

--- index/index.js
+++ index/index.js
@@ -4,13 +4,41 @@ $(document).ready(() => {
   const $url = $('#url')
   const $go = $('#go')
   const $web = $('#web')
+  const $code = $('#code')
 
   $web[0].addEventListener('ipc-message', event => {
     // prints "ping"
     console.log(event)
+    $code.text(getCode($url.val(),event.channel))
   })
 
   $web[0].addEventListener('did-stop-loading', ()=>{
-    $web[0].executeJavaScript('pingHost("ping",{a:1})')
+    //$web[0].executeJavaScript('pingHost("ping",{a:1})')
   })
+
+  $go.click(()=>{
+    $web[0].src = $url.val()
+  })
+
+  
+  function getCode(url,selector){
+    return `
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch()
+
+  const page = await browser.newPage()
+  await page.goto('${url}')
+
+  await page.waitForSelector('${selector}')
+  const $el = await page.$('${selector}')
+  let text = await page.evaluate(el => el.textContent, $el)
+  console.log(text)
+
+  await browser.close();
+})()
+    `
+  }
+
 })
--- index/webview-preload.js
+++ index/webview-preload.js
@@ -1,38 +1,63 @@
-const $ = require('jquery')
 const { ipcRenderer } = require('electron')
-
 window.pingHost = (channel, args) => {
   ipcRenderer.sendToHost(channel, args)
 }
 
-$(document).ready(() => {
-  let cur;
-  $(document).on("mouseenter", "*", function (e) {
+document.addEventListener("DOMContentLoaded", function (event) {
+  let cur
+  document.addEventListener('click', function (event) {
     if (!cur) {
+      cur = event.target
       highlight(cur)
       return
     }
 
-    if(cur === e.target){
+    if (event.target == cur) {
+      offHighlight(cur)
+      window.pingHost(getDomPath(cur))
+      cur = undefined
       return
     }
 
     offHighlight(cur)
-    cur = e.target
+    cur = event.target
     highlight(cur)
   })
 
-  $(document).on("click", "*", function (e) {
-    
-  })
 
   function highlight(el) {
-    $(el).css({
-      border: '1px solid red'
-    })
+    el.setAttribute('data-style', el.getAttribute('style') || '')
+    el.setAttribute('style', el.getAttribute('style') + ';border: 1px solid red;')
   }
 
   function offHighlight(el) {
-    $(el).attr('style', '')
+    el.setAttribute('style', el.getAttribute('data-style'))
   }
-})
+
+  function getDomPath(el) {
+    var stack = [];
+    while ( el.parentNode != null ) {
+      console.log(el.nodeName);
+      var sibCount = 0;
+      var sibIndex = 0;
+      for ( var i = 0; i < el.parentNode.childNodes.length; i++ ) {
+        var sib = el.parentNode.childNodes[i];
+        if ( sib.nodeName == el.nodeName ) {
+          if ( sib === el ) {
+            sibIndex = sibCount;
+          }
+          sibCount++;
+        }
+      }
+      if ( el.hasAttribute('id') && el.id != '' ) {
+        stack.unshift(el.nodeName.toLowerCase() + '#' + el.id);
+      } else if ( sibCount > 1 ) {
+        stack.unshift(el.nodeName.toLowerCase() + ':nth-child(' + (sibIndex+1) + ')');
+      } else {
+        stack.unshift(el.nodeName.toLowerCase());
+      }
+      el = el.parentNode;
+    }
+    return stack.slice(1).join(' > '); // removes the html element
+  }
+});
